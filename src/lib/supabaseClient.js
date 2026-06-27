f9af83a7c46e3e0c4958cc3dec5d3f0d54f691ad
Muahmmad Uzair <muzairk2020@gmail.com>
2026-06-27 07:26:16 -0700

    API Key Changed

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
+// src/lib/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://kvdqebadwgppyciaiich.supabase.co';
-const supabaseKey = 'sb_publishable_IkK78c6t1Nz9lusTe8wjaQ_KtnRE63G';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
